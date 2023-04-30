3a12d9b9282e408fc26d3cf4b610ea7fefd4fe52
digital-shouen <shouen.hisamatsu@gmail.com>
2023-04-30 14:03:31 +0900

    Lesson35kadai01

--- src/main/java/com/techacademy/controller/IndexController.java
+++ src/main/java/com/techacademy/controller/IndexController.java
@@ -2,14 +2,12 @@ package com.techacademy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RestController
-public class UserController {
+public class IndexController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello SpringBoot!";
+        return "index";
     }
 }
